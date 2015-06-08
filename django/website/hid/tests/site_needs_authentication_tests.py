d09104a08990188436d9f10df1ec798a25194111
Daniel Levy <daniell@aptivate.org>
2015-06-08 17:18:57 +0100

    Add ability to log in and out of site

--- django/website/hid/tests/site_needs_authentication_tests.py
+++ django/website/hid/tests/site_needs_authentication_tests.py
@@ -3,9 +3,18 @@ from django.core.urlresolvers import reverse
 from django.test.testcases import SimpleTestCase
 
 from .fast_dispatch import FastDispatchMixin
+from users.models import User
 
 
 class SiteNeedsAuthenticationTests(FastDispatchMixin, SimpleTestCase):
     def test_index_page_cant_be_accessed_when_not_logged_in(self):
         response = self.fast_dispatch('dashboard')
         self.assertEqual(settings.LOGIN_URL + '?next=' + reverse('dashboard'), response['Location'])
+
+    def test_index_page_can_be_accessed_when_logged_in(self):
+        self.user = User()
+        response = self.fast_dispatch('dashboard')
+
+        response.render()
+
+        self.assertIn('dashboard', response.content)
--- django/website/users/urls.py
+++ django/website/users/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 
 urlpatterns = patterns('',
     url(r'login/$', login, name="login"),
+    url(r'logout/$', login, name="logout"),
 )
