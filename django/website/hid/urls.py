87bdc4e9039482de6db8151a9b5116ac5ccc27b3
Marko Samastur <markos@aptivate.org>
2015-06-19 00:42:39 +0200

    Added skeleton/empty data view

--- django/website/hid/urls.py
+++ django/website/hid/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^sources/upload/$', login_required(UploadSpreadsheetView.as_view()), name='sources-upload'),
     url(r'^sources/(?P<label>\w+)/$', login_required(ListSources.as_view()), name='sources-edit'),
     url(r'^sources/$', login_required(ListSources.as_view()), name='sources'),
+    url(r'^view/$', login_required(TemplateView.as_view(template_name='hid/view.html')), name="data-view"),
     url(r'^$', login_required(TemplateView.as_view(template_name='hid/dashboard.html')), name="dashboard"),
 )
