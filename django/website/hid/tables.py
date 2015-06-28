ff9f621d6596e5ab4c9e1fd78da4942b051529b1
Marko Samastur <markos@aptivate.org>
2015-06-28 21:37:46 +0200

    Added checkbox column to items table & save button

--- django/website/hid/tables.py
+++ django/website/hid/tables.py
@@ -1,9 +1,17 @@
 import django_tables2 as tables
 
 
+class NamedCheckBoxColumn(tables.CheckBoxColumn):
+    @property
+    def header(self):
+        return self.verbose_name
+
+
 class ItemTable(tables.Table):
     timestamp = tables.Column(verbose_name="Created")
     body = tables.Column(verbose_name="Message")
+    delete = NamedCheckBoxColumn(accessor='id', verbose_name="Delete")
 
     class Meta:
         attrs = {"class": "table"}
+        template = "hid/table.html"
