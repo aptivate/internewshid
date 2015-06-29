4cfc2078ce3bead908d4cb86b049b860dba77361
Mark Skipper <marks@aptivate.org>
2015-06-29 12:18:26 +0100

    Order by created descending by default

--- django/website/hid/tables.py
+++ django/website/hid/tables.py
@@ -2,8 +2,10 @@ import django_tables2 as tables
 
 
 class ItemTable(tables.Table):
+    created = tables.Column(verbose_name="Imported")
     timestamp = tables.Column(verbose_name="Created")
     body = tables.Column(verbose_name="Message")
 
     class Meta:
         attrs = {"class": "table"}
+        order_by = ('-created',)
