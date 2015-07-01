1e8425e51df2751b551c013d813ce77a37e7659c
Mark Skipper <marks@aptivate.org>
2015-07-01 12:28:40 +0100

    Switch rest_api tests to pytest tests

--- django/website/rest_api/tests/item_view_tests.py
+++ django/website/rest_api/tests/item_view_tests.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals, absolute_import
 
-from django.test import TestCase
+import pytest
+
 from rest_framework.test import APIRequestFactory
 
 from data_layer.tests.factories import ItemFactory
@@ -8,24 +9,24 @@ from data_layer.tests.factories import ItemFactory
 from rest_api.views import ItemList
 
 
-class ItemViewTests(TestCase):
+def get(view_class, url):
+    view = view_class.as_view()
+    request = APIRequestFactory().get(url)
+    return view(request)
 
-    def get(self, view_class, url):
-        view = view_class.as_view()
-        request = APIRequestFactory().get(url)
-        return view(request)
+@pytest.mark.django_db
+def test_get_items_returns_empty_if_no_items():
+    response = get(ItemList, '/items/')
+    assert response.data == []
 
-    def test_get_items_returns_empty_if_no_items(self):
-        response = self.get(ItemList, '/items/')
-        self.assertEqual(response.data, [])
+@pytest.mark.django_db
+def test_get_items_returns_all_items():
+    item = ItemFactory()
 
-    def test_get_items_returns_all_items(self):
-        item = ItemFactory()
+    payload = get(ItemList, '/items/').data
 
-        payload = self.get(ItemList, '/items/').data
-
-        self.assertEqual(len(payload), 1)
-        self.assertEqual(payload[0]['body'], item.body)
-        # I don't much care for this test.
-        # It's testing too much of the stack, and its messy because the payload
-        # isn't the same kind data strcture even as ItemSerializer(item).data
+    assert len(payload) == 1
+    assert payload[0]['body'] == item.body
+    # I don't much care for this test.
+    # It's testing too much of the stack, and its messy because the payload
+    # isn't the same kind data strcture even as ItemSerializer(item).data
