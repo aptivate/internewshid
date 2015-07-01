894751487dc58042aa30c8b9548cdf9682f53bbf
Mark Skipper <marks@aptivate.org>
2015-07-01 21:12:08 +0100

    Use api views to back out transport

--- django/website/transport/data_layer_transport.py
+++ django/website/transport/data_layer_transport.py
@@ -1,9 +1,13 @@
+from rest_framework.test import APIRequestFactory
+
 from data_layer.handlers import Message
+from rest_api.views import ItemList
 
 
 def get_messages():
-    return Message.list()
-
+    request = APIRequestFactory().get('/items')
+    view = ItemList.as_view()
+    return view(request).data
 
 def create_message(message):
     Message.create(message)
