939ccb6c6a55a5d1758d1e1801e7c0965dc1d82b
Mark Skipper <marks@aptivate.org>
2015-07-01 21:38:16 +0100

    Add comment

--- django/website/transport/data_layer_transport.py
+++ django/website/transport/data_layer_transport.py
@@ -4,10 +4,10 @@ from data_layer.handlers import Message
 from rest_api.views import ItemList
 
 
-def get_messages():
+def get_messages():  # TODO rename get_items
     request = APIRequestFactory().get('/items')
     view = ItemList.as_view()
     return view(request).data
 
-def create_message(message):
+def create_message(message):  # TODO rename create_item
     Message.create(message)
