c9c83dabde4d44a22f37071ca1fdae309e0a25eb
Mark Skipper <marks@aptivate.org>
2015-07-05 21:18:21 +0100

    Make url names use 'item' instead of 'message'
    By default the router derives the url names from
    the model name (which it gets from the queryset)
    which is Message

--- django/website/rest_api/urls.py
+++ django/website/rest_api/urls.py
@@ -3,5 +3,9 @@ from rest_framework import routers
 from .views import ItemViewSet
 
 router = routers.SimpleRouter()
-router.register(r'items', ItemViewSet)
+router.register(
+    r'items',
+    ItemViewSet,
+    base_name='item'  # TODO: remove when Message model renamed Item
+)
 urlpatterns = router.urls
