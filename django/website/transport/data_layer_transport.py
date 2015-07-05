c5a8f0468fe2ab5220d6f4d2796bbff98fddbd30
Mark Skipper <marks@aptivate.org>
2015-07-05 22:30:08 +0100

    Rewrite list transport methods using ViewSet
    Actualy they are now also a class.
    The existing functions still exist and call into the class
    (wastefully creating an instance every time but I expect these
    to go away and rewrite the clients to use an instance of the
    transport class directly.

--- django/website/transport/data_layer_transport.py
+++ django/website/transport/data_layer_transport.py
@@ -1,25 +1,57 @@
+from django.core.urlresolvers import reverse
 from rest_framework.test import APIRequestFactory
 from rest_framework import status
-from rest_api.views import ItemList
+from rest_api.views import ItemViewSet
 
 from .exceptions import TransportException
 
 
+class ItemTransport(object):
+
+    url_name = 'item-list'
+    actions = {
+        'get': 'list',
+        'post': 'create',
+    }
+    request_factory = APIRequestFactory()
+
+    @property
+    def url(self):
+        return reverse(self.url_name)
+
+    @classmethod
+    def get_view(cls):
+        return ItemViewSet.as_view(actions=cls.actions)
+
+    def list(self, **kwargs):
+        """ Return a list of Items
+
+        If keyword arguments are given, they are used
+        to filter the Items.
+        """
+        # FIXME: currently only body exact filtering is supported
+        view = self.get_view()
+        request = self.request_factory.get(self.url, kwargs)
+        return view(request).data
+
+    def create(self, item):
+        """ Create an Item from the given dict """
+        view = self.get_view()
+        request = self.request_factory.post(self.url, item)
+        response = view(request)
+        if status.is_success(response.status_code):
+            return response.data
+        else:
+            response.data['status_code'] = response.status_code
+            raise TransportException(response.data)
+
+
 def get_messages(**kwargs):  # TODO rename get_items
-    request = APIRequestFactory().get('/items', kwargs)
-    view = ItemList.as_view()
-    return view(request).data
+    return ItemTransport().list(**kwargs)
 
 
 def create_message(item):  # TODO rename create_item
-    view = ItemList.as_view()
-    request = APIRequestFactory().post('/items', item)
-    response = view(request)
-    if status.is_success(response.status_code):
-        return response.data
-    else:
-        response.data['status_code'] = response.status_code
-        raise TransportException(response.data)
+    return ItemTransport().create(item)
 
 
 def delete_item(message_id):
