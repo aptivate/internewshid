fe9f110d56041bc11eeedc8bff54695293d37bf4
Mark Skipper <marks@aptivate.org>
2015-07-09 17:30:13 +0100

    Fix failing tests due to stale .pyc files

--- django/website/transport/tests/item_delete_tests.py
+++ django/website/transport/tests/item_delete_tests.py
@@ -2,26 +2,24 @@ from __future__ import unicode_literals, absolute_import
 import pytest
 
 from data_layer.tests.factories import ItemFactory
-from transport.data_layer_transport import ItemTransport
+from transport import items
 
 
 @pytest.mark.django_db
 def test_delete_item():
-    item_transport = ItemTransport()  # TODO: Should probably be a pytest fixture?
     item = ItemFactory()
-    assert len(item_transport.list()) == 1
+    assert len(items.list()) == 1
 
-    item_transport.delete(item.id)
+    items.delete(item.id)
 
-    assert len(item_transport.list()) == 0
+    assert len(items.list()) == 0
 
 
 @pytest.mark.django_db
 def test_delete_items():
-    item_transport = ItemTransport()  # TODO: Should probably be a pytest fixture?
     ids = [ItemFactory().id for i in range(10)]
-    assert len(item_transport.list()) == 10
+    assert len(items.list()) == 10
 
-    item_transport.bulk_delete(ids)
+    items.bulk_delete(ids)
 
-    assert len(item_transport.list()) == 0
+    assert len(items.list()) == 0
