ea2b0f505987bedc241661deda617a8fee5f92c2
Alice Heaton <aliceh@aptivate.org>
2015-07-10 14:53:42 +0100

    Add backbone and underscore, and re-write flot chart to use backbone models and views.

--- django/website/hid/static/hid/widgets/chart.js
+++ django/website/hid/static/hid/widgets/chart.js
@@ -1,51 +1,72 @@
 (function($) {
     /**
-     * Class used to display flot charts.
+     * FlotChart model
      *
-     * Flot charts are displayed in the specified element,
-     * which should contain two data properties:
+     * This contains the data and options of a
+     * single flot chart.
      *
-     * - data-data: Containing the chart data
-     * - data-options: Containing the chart options
      */
-    function HidFlotChart(element) {
-        var self = this;
+    var FlotChart = Backbone.Model.extend({
+        defaults: {
+            data: [],
+            options: {}
+        }
+    });
 
-        self.initialize = function() {
-            // Set up the element
-            self.$element = $(element);
-            self.$container = self.$element.closest('.panel-body');
+    /**
+     * FlotChartView view
+     *
+     * Displays a single FlotChart model
+     */
+    var FlotChartView = Backbone.View.extend({
+        /* Create the view */
+        initialize: function(options) {
+            this.chart = options.chart;
+            this.$container = this.$el.closest('.panel-body');
+            $(window).on('resize', _.bind(this.resize, this));
+        },
 
-            // Read the element's data
-            var data_values = self.$element.data();
-            self.data = data_values['data'];
-            self.options = data_values['options'];
+        /* Remove the view */
+        remove: function() {
+            $(window).off('resize', this.resize);
+            this.$el.remove();
+        },
 
-            // Setup event handling
-            $(window).resize(function(){
-                self.resize();
-            });
-            self.resize();
+        /* Render the chart */
+        render: function() {
+            this.resize();
+            $.plot(
+                this.$el,
+                this.chart.get('data'), 
+                this.chart.get('options')
+            );
+            return this;
+        },
 
-            // And plot
-            $.plot(self.$element, self.data, self.options);
-        };
+        /* Resize the chart */
+        resize: function() {
+            this.$el.width(this.$container.width());
+            this.$el.height(this.$container.height());
+        },
+    });
 
-        /*
-         * Resize the chart to the container's size.
-         */
-        self.resize = function() {
-            self.$element.width(self.$container.width());
-            self.$element.height(self.$container.height());
-        };
-
-
-        self.initialize();
-    }
-
+    /**
+     * Create a flot chart for every matching element.
+     *
+     * The elements are execpted to have a class 'flot-chart'
+     * and two data attributes:
+     *
+     *     data: flot data
+     *     options: flot options
+     */
     $(document).ready(function() {
         $('.flot-chart').each(function() {
-            HidFlotChart($(this));
+            var flot_chart = new FlotChart($(this).data());
+            var flot_chart_view = new FlotChartView({
+                el: this,
+                chart: flot_chart
+            });
+            flot_chart_view.render();
         });
     });
 })(jQuery);
