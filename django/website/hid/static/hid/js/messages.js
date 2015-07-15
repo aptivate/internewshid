b70cd3f7c98594dba85c64e98f1dcf390e7804f7
Jay Alvarez <jaya@aptivate.org>
2015-07-15 15:53:03 +0100

    messages box messages hide after 3 seconds

---
+++ django/website/hid/static/hid/js/messages.js
@@ -0,0 +1,10 @@
+/**
+ * Hide messages box after 3 seconds automatically
+ */
+
+jQuery(window).load(function(){
+  setTimeout(function(){ 
+      jQuery('.messages-box > ul').fadeOut();
+      }, 3000);
+});
+
