4ce45dc7774f1d0b63ef52c4d164ba89de6fd282
Mark Skipper <marks@aptivate.org>
2015-07-16 21:47:46 +0100

    Add test for transport.item.create failure
    Throws exception if important fields are missing.

--- django/website/transport/tests/item_create_tests.py
+++ django/website/transport/tests/item_create_tests.py
@@ -1,15 +1,19 @@
 from __future__ import unicode_literals, absolute_import
+from django.utils import timezone
 import pytest
 
 from transport import items
-from django.utils import timezone
+from ..exceptions import TransportException
+
+
+@pytest.fixture
+def now():
+    return timezone.now().replace(microsecond=0)
+    # MySQL discards microseconds
 
 
 @pytest.mark.django_db
-def test_create_message_creates_item():
-    now = timezone.now().replace(
-        microsecond=0  # MySQL discards microseconds
-    )
+def test_create_item_creates_item(now):
     item = {'body': "Text", 'timestamp': now}
     old_count = len(items.list())
 
@@ -18,3 +22,9 @@ def test_create_message_creates_item():
     assert 'id' in response
     new_count = len(items.list())
     assert new_count > old_count
+
+
+def test_create_item_throws_exception_for_missing_body():
+    with pytest.raises(TransportException) as excinfo:
+        items.create({})
+    assert 'body' in excinfo.value.message
