aabab489c157d37e6c73dd2190aca7c1f4e0f303
Mark Skipper <marks@aptivate.org>
2015-07-16 16:35:53 +0100

    Make ItemSerializer.terms a nested relationship
    Had to make the nested serializer writable to stop other tests failing
    (and because I think we need this later for creating tagged items on
    import)
    Also moved some meta classes up to top of classes
    to make them easier to parse.

--- django/website/rest_api/serializers.py
+++ django/website/rest_api/serializers.py
@@ -9,26 +9,61 @@ from taxonomies.models import (
 )
 
 
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-
-
 class TaxonomySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Taxonomy
+
     slug = serializers.SlugField(
         required=False,
         # FIXME: max_length=250, causes AppRegistryNotReady excpetion!" Go figure
     )
 
-    class Meta:
-        model = Taxonomy
 
 
 class TermSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Term
+
     taxonomy = serializers.SlugRelatedField(
         slug_field='slug',
         queryset=Taxonomy.objects.all()
     )
 
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = Term
+        model = Item
+
+    terms = TermSerializer(many=True)
+
+    def create(self, validated_data):
+        """ Create an item with nested metadata terms
+        The validated data looks something like this:
+        { "body": "some text",
+            "terms": [
+                {"taxonomy": "animal", "name": "Dog"},
+                {"taxonomy": "thing", "name": "foo"}
+            ],
+        }
+
+        """
+        # find all terms listed in term_data and link to item
+        # in future, we might theoreteically be adding new tags here too,
+        # in which case there will be more edge cases to detect
+        # because you're only allowed to add if the taxonomy allows it
+        # when we add that feature to taxonomies.
+        term_list = validated_data.pop('terms', [])
+        item = Item.objects.create(**validated_data)
+        for term_data in term_list:
+            term = Term.objects.get(
+                taxonomy=term_data['taxonomy'],
+                name=term_data['name'],
+            )
+            item.terms.add(term)
+        return item
+
+
