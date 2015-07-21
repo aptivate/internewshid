14ad22427e10fd6202a3132e51f59c2ca8fccca1
Alice Heaton <aliceh@aptivate.org>
2015-07-21 16:08:50 +0100

    Replace the save changes button with a drop down list of actions and an update button. Actions include deleting and setting a question type on all selected items.

--- django/website/hid/views.py
+++ django/website/hid/views.py
@@ -11,12 +11,15 @@ from django_tables2 import SingleTableView
 from chn_spreadsheet.importer import Importer, SheetImportException
 from data_layer.models import Term
 import transport
+from transport.exceptions import TransportException
 from .assets import require_assets
 from .forms import UploadForm, get_spreadsheet_choices
 from .tables import ItemTable
 
 
 QUESTION_TYPE_TAXONOMY = 'ebola-questions'
+ADD_CATEGORY_PREFIX = 'add-category-'
+DELETE_COMMAND = 'command'
 
 
 class ListSources(TemplateView):
@@ -102,25 +105,64 @@ class ViewItems(SingleTableView):
         context = super(ViewItems, self).get_context_data(**kwargs)
         context['type_label'] = _('Questions')
         context['upload_form'] = UploadForm(initial={'source': 'geopoll'})
+        context['actions'] = [
+            self._build_action_dropdown_group(
+                items=[(DELETE_COMMAND, _('Delete Selected'))]
+            ),
+            self._build_action_dropdown_group(
+                label=_('Set question type'),
+                items=self.get_category_options(),
+                prefix=ADD_CATEGORY_PREFIX
+            )
+        ]
+
         require_assets('hid/js/automatic_file_upload.js')
         require_assets('hid/js/select_all_checkbox.js')
         return context
 
+    def _build_action_dropdown_group(self, label='', items=[], prefix=''):
+        """ Helper method to build a group of actions used in the
+            action dropdown.
 
-def get_deleted(params):
-    return [int(x) for x in params.getlist("delete", [])]
+            Args:
+                - label: Label of the group of action;
+                - items: List of items in the group. Each item is a tupple
+                         consisting of the command suffix and the display
+                         name;
+                - prefix: A string used to prefix the command string.
+
+            Returns:
+                A dictionary representing the action group.
+        """
+        return {
+            'label': label,
+            'items': dict(
+                [(prefix + k, v) for k, v in items]
+            )
+        }
 
 
-def get_categories(params, deleted_ids=[]):
-    removed = set(deleted_ids)
+def get_selected(params):
+    """ Given a request POST/GET element, return the list
+        of items defined by 'select_action'.
 
-    categories = [(int(key[9:]), val)
-                  for key, val in params.items()
-                  if key.startswith("category-")]
-    return [cat for cat in categories if cat[1] and cat[0] not in removed]
+        Args:
+            - params: POST/GET object
+
+        Returns:
+            A list of item ids.
+    """
+    return [int(x) for x in params.getlist("select_action", [])]
 
 
 def delete_items(request, deleted):
+    """ Delete the given items, and set a success/failure
+        on the request
+
+        Args:
+            - request: Current request object
+            - items: List of items to delete
+    """
     try:
         transport.items.bulk_delete(deleted)
         num_deleted = len(deleted)
@@ -133,38 +175,61 @@ def delete_items(request, deleted):
         messages.error(request, msg)
 
 
-def add_categories(categories):
-    """ Add specified category Terms to The items
-    as specified in categories list.
+def add_items_categories(request, items, category):
+    """ Add the given category to the given items,
+        and set a success/failure on the request
 
-    args:
-        categories: a list of item ids and term names:
-            [ (<item-id>, <term-name>), ... ]
+        Args:
+            - request: Current request object
+            - items: List of item ids to add the category too
+            - category: Category name to add
     """
-    for item_id, term_name in categories:
-        transport.items.add_term(
-            item_id,
-            QUESTION_TYPE_TAXONOMY,
-            term_name,
-        )
-    # Did we want to test for any failures or exceptions ?
-    # TODO: Add messages/success/error reporting here?
+    success = 0
+    failed = 0
+    for item_id in items:
+        try:
+            transport.items.add_term(
+                item_id,
+                QUESTION_TYPE_TAXONOMY,
+                category,
+            )
+            success += 1
+        except TransportException:
+            failed += 1
+    if success > 0:
+        msg = ungettext("Successfully updated %d item.",
+                        "Successfully updated %d items.",
+                        len(items)) % len(items)
+        messages.success(request, msg)
+    if failed > 0:
+        msg = ungettext("Failed to update %d item.",
+                        "Failed to update %d items.",
+                        len(items)) % len(items)
+        messages.success(request, msg)
 
 
 def process_items(request):
-    '''
-    If POST request, then:
-    - delete items that were checked
-    - update categories on those that weren't deleted.
-    '''
+    """ Request to process a selection of items from the
+        view & edit page.
+
+        Args:
+            - request: Request object. This should contain
+              a POST request defining:
+                  - action: The action to apply
+                  - select_action: List of items to apply
+                    the action too.
+    """
     redirect_url = reverse("data-view")
     # Just redirect back to items view on GET
     if request.method == "POST":
-        deleted = get_deleted(request.POST)
-        categories = get_categories(request.POST, deleted)
-        if len(deleted):
-            delete_items(request, deleted)
-        if len(categories):
-            add_categories(categories)
+        selected = get_selected(request.POST)
+        action = request.POST.get('action')
+        if action == DELETE_COMMAND:
+            delete_items(request, selected)
+        elif action.startswith(ADD_CATEGORY_PREFIX):
+            category = action[len(ADD_CATEGORY_PREFIX):]
+            add_items_categories(request, selected, category)
+        else:
+            messages.error(request, _('Unknown action'))
 
     return HttpResponseRedirect(redirect_url)
