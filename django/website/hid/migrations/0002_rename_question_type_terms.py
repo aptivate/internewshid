aaf3a907d7763b0f5f6cf9688efbd317a872f2ee
Mark Skipper <marks@aptivate.org>
2015-07-23 11:17:50 +0100

    Migration to delete old category Terms and create new ones
    https://trello.com/c/Q9hEiE20/83-change-categories-available-to-match-those-specified-in-the-story

--- django/website/hid/migrations/0002_rename_question_type_terms.py
+++ django/website/hid/migrations/0002_rename_question_type_terms.py
@@ -3,51 +3,32 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+QUESTION_TYPES = (
+    ('Ebola updates', 'What are the current updates on Ebola?'),
+    ('Ebola authenticity', 'Is Ebola a real disease?'),
+    ('Ebola prevention', 'What measures could be put in place to end Ebola?'),
+    ('Ebola origins', 'What is the origin of Ebola?'),
+    ('Non-Ebola concerns', 'What are the non-Ebola related concerns?'),
+    ('Ebola symptons', 'What are the symptoms of Ebola?'),
+    ('Ebola vaccine', 'What are the stakes of the Ebola vaccine?'),
+    ('Liberia Ebola-free', 'Can Liberia be Ebola free?'),
+    ('Unknown', 'Unknown.'),
+)
 
 def rename_question_type_terms(apps, schema_editor):
+    Taxonomy = apps.get_model('taxonomies', 'Taxonomy')
     Term = apps.get_model('taxonomies', 'Term')
-
-    old_question_names = (
-        "Vaccine",
-        "Measures",
-        "Symptoms",
-        "Stigmatism",
-        "Victims",
-        "Schools",
-        "Updates",
-        "Origin",
-        "Others",
-        "Real",
+    (taxonomy, _) = Taxonomy.objects.get_or_create(
+        slug="ebola-questions",
+        name="Ebola Questions",
     )
+    Term.objects.filter(taxonomy=taxonomy).delete()
+    new_terms = [
+        Term(name=qt[0], long_name=qt[1], taxonomy=taxonomy)
+        for qt in QUESTION_TYPES
+    ]
 
-    new_question_types = (
-        ('Ebola updates', 'What are the current updates on Ebola?'),
-        ('Ebola authenticity', 'Is Ebola a real disease?'),
-        ('Ebola prevention', 'What measures could be put in place to end Ebola?'),
-        ('Ebola origins', 'What is the origin of Ebola?'),
-        ('Non-Ebola concerns', 'What are the non-Ebola related concerns?'),
-        ('Ebola symptons', 'What are the symptoms of Ebola?'),
-        ('Ebola vaccine', 'What are the stakes of the Ebola vaccine?'),
-        ('Liberia Ebola-free', 'Can Liberia be Ebola free?'),
-        ('Unknown', 'Unknown.'),
-    )
-
-    rename_map = map(
-        lambda *args: tuple(args),
-        old_question_names,
-        new_question_types,
-    ) # zip, but with None for missing new questions
-
-    for (old_name, new_question) in rename_map:
-        term = Term.objects.get(name=old_name)
-        if new_question:
-            new_name, new_long_name = new_question
-            term = Term.objects.get(name=old_name)
-            term.name = new_name
-            term.long_name = new_long_name
-            term.save()
-        else:
-            term.delete()
+    Term.objects.bulk_create(new_terms)
 
 
 class Migration(migrations.Migration):
