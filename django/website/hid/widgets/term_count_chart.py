8fc6441ae6d3e612b109b20e0eeacb01a75d346a
Alice Heaton <aliceh@aptivate.org>
2015-07-23 16:49:12 +0100

    Rename QuestionChartWidget to TermCountChartWidget as this is what it does, and pull the actual data from the transport layer.

--- django/website/hid/widgets/term_count_chart.py
+++ django/website/hid/widgets/term_count_chart.py
@@ -1,13 +1,20 @@
-class QuestionChartWidget(object):
-    """ A horizontal bar chart used to display quantitative answers to
-        specific questions.
+from collections import OrderedDict
+from django.utils.translation import ugettext_lazy as _
+from transport.taxonomies import term_itemcount
 
-        Eventually this should pull the questions and values from the
-        data API. For now we pass these in via the settings.
+
+class TermCountChartWidget(object):
+    """ A horizontal bar chart used to display number of entries for each
+        term of a taxonomy.
 
         Settings:
-            name: Name of the chart
-            questions: Dictionary of question to value
+            title: Name of the widget
+            taxonomy: Slug of the taxonomy
+            count: Maximum number of terms to display.
+                   If this is >0, then only the countth most
+                   used terms are displayed, and all others
+                   are aggregated under 'Others'
+            other_label: Optional label to use instead of 'Others'
     """
     template_name = 'hid/widgets/chart.html'
     javascript = [
@@ -16,17 +23,65 @@ class QuestionChartWidget(object):
         'hid/widgets/chart.js'
     ]
 
-    def get_context_data(self, **kwargs):
-        index = 0
+    def _fetch_counts(self, taxonomy, count, other_label):
+        """ Given a taxonomy, fetch the count per term.
+
+        Args:
+            - taxonomy: Taxonomy slug
+            - count: If >0, maximum number of rows to returns. If the data
+                     has more terms, all other terms are aggregated under
+                     an 'others' section
+            - other_label: Label for the 'Others' section
+        """
+        itemcount = term_itemcount(taxonomy)
+        itemcount.sort(key=lambda k: int(k['count']), reverse=True)
+        if count > 0:
+            head = itemcount[0:count-1]
+            tail = itemcount[count-1:]
+        else:
+            head = itemcount
+            tail = []
+        head.sort(key=lambda k: k['long_name'])
+        counts = OrderedDict()
+        for item in head:
+            counts[item['long_name']] = item['count']
+        if len(tail) > 0:
+            agg = 0
+            for item in tail:
+                agg = agg + item['count']
+            counts[other_label] = agg
+        return counts
+
+    def _create_axis_values(self, counts):
+        """ Given a dictionary of label to value, create the Y and X axis values
+            to be used in flot.
+
+        Args:
+            - chart: A dictionary of label to value
+
+        Returns:
+           A tuple containing (X Axis data, Y Axis data)
+        """
         yticks = []
         values = []
-        for question, answer in kwargs['questions'].items():
-            yticks.append([index, question])
-            values.append([answer, index])
-            index += 1
+        index = len(counts)
+        for label, value in counts.items():
+            yticks.append([index, label])
+            values.append([value, index])
+            index -= 1
 
+        return values, yticks
+
+    def get_context_data(self, **kwargs):
+        title = kwargs.get('title', _('(missing title)'))
+        taxonomy = kwargs.get('taxonomy')
+        count = kwargs.get('count', 0)
+        other_label = kwargs.get('other_label', 'Others')
+
+        counts = self._fetch_counts(taxonomy, count, other_label)
+        (values, yticks) = self._create_axis_values(counts)
         return {
-            'name': kwargs['name'],
+            'title': title,
             'options': {
                 'series': {
                     'bars': {
