258cb9c51863a9e0f6fde272c09727cb91fc9ca0
Mark Skipper <marks@aptivate.org>
2015-07-23 14:47:17 +0100

    Rename Sympton to symptom +...
    remove unused import and fix pep8 white space

--- django/website/hid/migrations/0002_rename_question_type_terms.py
+++ django/website/hid/migrations/0002_rename_question_type_terms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 
 QUESTION_TYPES = (
     ('Ebola updates', 'What are the current updates on Ebola?'),
@@ -9,11 +9,12 @@ QUESTION_TYPES = (
     ('Ebola prevention', 'What measures could be put in place to end Ebola?'),
     ('Ebola origins', 'What is the origin of Ebola?'),
     ('Non-Ebola concerns', 'What are the non-Ebola related concerns?'),
-    ('Ebola symptons', 'What are the symptoms of Ebola?'),
+    ('Ebola symptoms', 'What are the symptoms of Ebola?'),
     ('Ebola vaccine', 'What are the stakes of the Ebola vaccine?'),
     ('Liberia Ebola-free', 'Can Liberia be Ebola free?'),
     ('Unknown', 'Unknown.'),
 )
+
 
 def rename_question_type_terms(apps, schema_editor):
     Taxonomy = apps.get_model('taxonomies', 'Taxonomy')
