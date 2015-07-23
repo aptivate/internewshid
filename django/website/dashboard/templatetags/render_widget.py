596cf4f7c558276c1edd02b99d432ecf0c82c0a0
Alice Heaton <aliceh@aptivate.org>
2015-07-23 16:17:27 +0100

    Ensure dashboard displays error in case of missing or faulty widget, rather than causing the whole application to fail.

--- django/website/dashboard/templatetags/render_widget.py
+++ django/website/dashboard/templatetags/render_widget.py
@@ -1,9 +1,13 @@
+import logging
+
 from django import template
 from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
 
-from dashboard.widget_pool import get_widget
+from dashboard.widget_pool import get_widget, MissingWidgetType
 
 
+logger = logging.getLogger(__name__)
 register = template.Library()
 
 
@@ -16,18 +20,40 @@ def render_widget(widget_instance):
     get_context_data then that is used to generate the
     template context.
     """
-    widget = get_widget(widget_instance.widget_type)
+    widget = None
+    # Get settings, if any
     if widget_instance.settings:
         settings = widget_instance.settings
     else:
         settings = {}
+    # Get widget
     try:
-        context = widget.get_context_data(**settings)
-    except AttributeError:
+        widget = get_widget(widget_instance.widget_type)
+    except MissingWidgetType:
+        template_name = 'dashboard/widget-error.html'
         context = {}
-    try:
-        template_name = widget.template_name
-    except AttributeError:
-        template_name = 'dashboard/widget-missing-template.html'
+        context['error'] = _('Unknown widget type %(widget_type)s') % {
+            'widget_type': widget_instance.widget_type
+        }
         context['empty_type'] = widget_instance.widget_type
+    if widget:
+        # Get template
+        try:
+            template_name = widget.template_name
+        except AttributeError:
+            template_name = 'dashboard/widget-error.html'
+            context = {}
+            context['error'] = _('Missing template for %(widget_type)s') % {
+                'widget_type': widget_instance.widget_type
+            }
+            widget = None
+    if widget:
+        # Get context
+        try:
+            context = widget.get_context_data(**settings)
+        except:
+            logger.exception()
+            template_name = 'dashboard/widget-error.html'
+            context['error'] = _('Widget error. See error logs.')
+
     return render_to_string(template_name, context)
