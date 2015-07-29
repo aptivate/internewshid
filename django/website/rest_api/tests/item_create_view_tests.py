d7d32540c9c7b76545a34c64393017df7414a4ee
Martin Burchell <martinb@aptivate.org>
2015-07-29 16:37:59 +0100

    Improved debugging from test function

--- django/website/rest_api/tests/item_create_view_tests.py
+++ django/website/rest_api/tests/item_create_view_tests.py
@@ -12,7 +12,10 @@ from rest_api.views import ItemViewSet
 def create_item(**kwargs):
     request = APIRequestFactory().post('/items', kwargs)
     view = ItemViewSet.as_view(actions={'post': 'create'})
-    return view(request)
+    response = view(request)
+    assert status.is_success(response.status_code), response.data
+
+    return response
 
 
 @pytest.mark.django_db
