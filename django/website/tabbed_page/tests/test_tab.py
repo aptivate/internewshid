e4708bfbeae2e125aec017a5a4b5ff8a48dc014d
Martin Burchell <martinb@aptivate.org>
2015-08-06 12:03:08 +0100

    Refactored tests to use basic html tab

--- django/website/tabbed_page/tests/test_tab.py
+++ django/website/tabbed_page/tests/test_tab.py
@@ -2,18 +2,7 @@ import pytest
 
 
 class TestTab(object):
-    def __init__(self, template_name=None, context=None):
-        self.template_name = template_name
-
-        if context is None:
-            context = {}
-
-        self.context = context
-
-    def get_context_data(self, **kwargs):
-        context = getattr(self, 'context', {})
-
-        return context
+    pass
 
 
 @pytest.fixture
