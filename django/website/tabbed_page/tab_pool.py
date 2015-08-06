417bf07be0088dce6a0c3d5432df8875dbd6b5a9
Alice Heaton <aliceh@aptivate.org>
2015-08-06 11:14:21 +0100

    Add a basic html tab for tabs that just contain text (and for testing)

--- django/website/tabbed_page/tab_pool.py
+++ django/website/tabbed_page/tab_pool.py
@@ -24,3 +24,26 @@ def clear_tabs():
     global _pool
 
     _pool = {}
+
+
+class BasicHtmlTab(object):
+    """ A simple tab to display html
+
+    Settings:
+        title: The title of the tab
+            (in the header, not in
+             the tab)
+        body: The tab html
+    """
+    template_name = 'tabbed_page/basic_html_tab.html'
+
+    def get_context_data(self, **kwargs):
+        title = kwargs.get('title', '(no title)')
+        body = kwargs.get('body', '(no body)')
+        return {
+            'title': title,
+            'body': body
+        }
+
+
+register_tab('basic-html-tab', BasicHtmlTab())
