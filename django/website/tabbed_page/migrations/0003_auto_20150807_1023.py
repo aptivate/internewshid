ea6caeabe07485eb988d6a85680c0bbbd984369b
Alice Heaton <aliceh@aptivate.org>
2015-08-07 12:17:59 +0100

    Added missing migration file for previous commit.

---
+++ django/website/tabbed_page/migrations/0003_auto_20150807_1023.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tabbed_page', '0002_auto_20150805_1521'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='tabinstance',
+            old_name='view_name',
+            new_name='tab_type',
+        ),
+        migrations.AlterField(
+            model_name='tabbedpage',
+            name='name',
+            field=models.SlugField(unique=True, max_length=128),
+        ),
+        migrations.AlterField(
+            model_name='tabinstance',
+            name='name',
+            field=models.SlugField(max_length=128),
+        ),
+        migrations.AlterField(
+            model_name='tabinstance',
+            name='page',
+            field=models.ForeignKey(related_name='tabs', to='tabbed_page.TabbedPage'),
+        ),
+    ]
