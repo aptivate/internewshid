da1ff953edb70fe943fc186b9fb1a3a0a35436bd
Martin Burchell <martinb@aptivate.org>
2015-08-11 15:32:04 +0100

    Separated delete terms tests into own file

---
+++ django/website/rest_api/tests/delete_all_terms_tests.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals, absolute_import
+
+import pytest
+
+from django.core.urlresolvers import reverse
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from data_layer.models import Item
+from .item_create_view_tests import create_item
+from .taxonomy_and_term_create_tests import (
+    create_category,
+    add_term,
+)
+from .categorize_items_tests import (
+    categorize_item,
+    category,
+    item,
+    term
+)
+from ..views import ItemViewSet
+
+
+def remove_categories_from_item(item, taxonomy):
+    url = reverse('item-delete-all-terms', kwargs={"pk": item['id']})
+    request = APIRequestFactory().post(url, {'taxonomy': taxonomy})
+    view = ItemViewSet.as_view(actions={'post': 'delete_all_terms'})
+    return view(request, item_pk=item['id'])
+
+
+@pytest.mark.django_db
+def test_all_item_categories_can_be_deleted(item, term):
+    categorize_item(item, term)
+
+    # TODO: use the API for this
+    [item_orm] = Item.objects.all()
+    terms = item_orm.terms.all()
+    assert len(terms) == 1
+
+    remove_categories_from_item(item, term['taxonomy'])
+
+    terms = item_orm.terms.all()
+    assert len(terms) == 0
