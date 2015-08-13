c9b2fc0ce9a17da506aba344979b1542485a1671
Mark Skipper <marks@aptivate.org>
2015-08-13 15:03:31 +0100

    Use transport layer to create item in tests
    Responding to feedback from code review, change the item fixture
    to use the create method from transport.items instead of FactoryBoy

--- django/website/transport/tests/item_detail_tests.py
+++ django/website/transport/tests/item_detail_tests.py
@@ -2,28 +2,25 @@ from __future__ import unicode_literals, absolute_import
 
 import pytest
 
-from data_layer.tests.factories import ItemFactory
-import transport
+from .. import items
 from ..exceptions import TransportException
 
 
 @pytest.fixture
 def item():
-    return ItemFactory()
+    return items.create({'body': "Text"})
 
 
 @pytest.mark.django_db
 def test_get_item_gets_item(item):
-    item_data = transport.items.get(item.id)
-
-    assert item_data['id'] == item.id
-    assert item_data['body'] == item.body
+    item_data = items.get(item['id'])
+    assert all(item_data[key] == item[key] for key in ['id', 'body'])
 
 
 @pytest.mark.django_db
 def test_get_item_throws_exception_for_unknown_id():
     UNKNOWN_ITEM_ID = 6  # I am not a prisoner
     with pytest.raises(TransportException) as excinfo:
-        item_data = transport.items.get(UNKNOWN_ITEM_ID)
+        items.get(UNKNOWN_ITEM_ID)
 
     assert excinfo.value.message['detail'] == 'Not found.'
