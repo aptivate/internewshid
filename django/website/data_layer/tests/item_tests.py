15d45df6ac31db96b8cea6f510e625e11f5ef381
Martin Burchell <martinb@aptivate.org>
2015-08-19 17:10:30 +0100

    Enabled data_layer test for applying multiple tags

--- django/website/data_layer/tests/item_tests.py
+++ django/website/data_layer/tests/item_tests.py
@@ -130,15 +130,14 @@ def test_apply_term_replaces_term_for_categories():
     item.apply_term(term2)
     assert list(item.terms.all()) == [term2]
 
-@pytest.mark.xfail
-# I'm putting this here to explain some of my thinking.
+
 @pytest.mark.django_db
 def test_apply_term_adds_term_for_tags():
     item = ItemFactory()
-    taxonomy = TaxonomyFactory()  # Ensure multiplicity == multiple
+    taxonomy = TaxonomyFactory(multiplicity='multiple')
     term1 = TermFactory(taxonomy=taxonomy)
     term2 = TermFactory(taxonomy=taxonomy)
-    assert taxonomy.is_multiple
+    assert not taxonomy.is_optional
 
     item.apply_term(term1)
     assert list(item.terms.all()) == [term1]
