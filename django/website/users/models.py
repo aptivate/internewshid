ff87d7df86f3e45d93cf92d23f3345bd74c64e21
Martin Burchell <martinb@aptivate.org>
2015-08-21 17:51:10 +0100

    Changed business email label to Email

--- django/website/users/models.py
+++ django/website/users/models.py
@@ -55,7 +55,7 @@ class UserManager(BaseUserManager):
 # Models
 class User(AbstractBaseUser, PermissionsMixin):
     # Account info
-    business_email = EmailField(unique=True)
+    business_email = EmailField(unique=True, verbose_name='Email')
     is_staff = BooleanField(
         verbose_name='staff status',
         default=False,
