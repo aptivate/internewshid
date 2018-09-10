55cbcb4e3b056ab9e30a8c2a1d03178343d95199
Luke Murphy <lukewm@riseup.net>
2018-09-10 15:07:50 +0200

    Remove all that settings noise.

--- django/website/settings.py
+++ django/website/settings.py
@@ -1,31 +1,17 @@
-# Django settings for  project.
-
-# Build paths inside the project like this: path.join(BASE_DIR, ...)
 from __future__ import unicode_literals, absolute_import
 
 from os import path
 BASE_DIR = path.abspath(path.dirname(__file__))
 
 
-########## DEFAULT DEBUG SETTINGS - OVERRIDE IN local_settings
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-##########
 
 
-########## DATABASES are configured in local_settings.py.*
-
-
-########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 import private_settings
 SECRET_KEY = private_settings.SECRET_KEY
-########## END SECRET CONFIGURATION
 
 
-########## MANAGER/EMAIL CONFIGURATION
-# These email addresses will get all the error email for the production server
-# (and any other servers with DEBUG = False )
 ADMINS = (
     ('Aptivate internewshid team', 'internewshid-team@aptivate.org'),
     ('Mark Skipper', 'internewshid-team@aptivate.org'),  # this is in case the above email doesn't work
@@ -33,104 +19,57 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# these are the settings for production. We can override in the various
-# local_settings if we want to
 DEFAULT_FROM_EMAIL = 'donotreply@internewshid.aptivate.org'
 SERVER_EMAIL = 'server@internewshid.aptivate.org'
-########## MANAGER/EMAIL CONFIGURATION
 
-
-########## GENERAL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
 TIME_ZONE = 'Europe/London'
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en'
 
 LANGUAGES = [
     ('en', 'English'),
 ]
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
-# If this is set to True, the datetime settings below will be overridden
-# by the locale settings
 USE_L10N = False
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
 USE_TZ = True
 
 SHORT_DATETIME_FORMAT = 'd M Y H:i'
 SHORT_DATE_FORMAT = 'd M Y'
-# TODO this is used in hid/tables.py
-# and should probably use FORMAT_MODULE_PATH instead.?
 
-########## END GENERAL CONFIGURATION
-
-
-########## MEDIA CONFIGURATION
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
 MEDIA_ROOT = path.join(BASE_DIR, 'uploads')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/uploads/'
-########## END MEDIA CONFIGURATION
 
-
-########## STATIC FILE CONFIGURATION
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = path.join(BASE_DIR, 'static')
 
-# URL prefix for static files.
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
-# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     path.join(BASE_DIR, 'media'),
 )
 
-# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django_assets.finders.AssetsFinder'
 )
-########## END STATIC FILE CONFIGURATION
 
 LOCALE_DIR = path.join(BASE_DIR, 'locale')
 if path.isdir(LOCALE_DIR):
     LOCALE_PATHS = (LOCALE_DIR,)
 
-########## APP CONFIGURATION
 DJANGO_APPS = (
-    # Default Django apps:
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Useful template tags:
-    # 'django.contrib.humanize',
-
-    # Admin
     'django.contrib.admin',
 )
 
@@ -161,10 +100,7 @@ DATA_LAYER_APPS = (
 
 INSTALLED_APPS = DATA_LAYER_APPS + LOCAL_APPS + THIRD_PARTY_APPS + DJANGO_APPS
 
-########## END APP CONFIGURATION
 
-
-########## MIDDLEWARE CONFIGURATION
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -173,18 +109,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-########## END MIDDLEWARE CONFIGURATION
 
-
-########## URL Configuration
 ROOT_URLCONF = 'urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
-# WSGI_APPLICATION = 'wsgi.application'
-########## END URL Configuration
-
-
-########## django-secure - intended for sites that use SSL
 SECURE = False
 if SECURE:
     INSTALLED_APPS += ("djangosecure",)
@@ -198,52 +125,25 @@ if SECURE:
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
     SECURE_SSL_REDIRECT = True
-########## end django-secure
 
-
-########## AUTHENTICATION CONFIGURATION
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
-    # "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-# Some really nice defaults
-# ACCOUNT_AUTHENTICATION_METHOD = "username"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-########## END AUTHENTICATION CONFIGURATION
 
-
-########## Custom user app defaults
-# Select the correct user model
 AUTH_USER_MODEL = "users.User"
 EMAIL_BOT_ADDRESS = 'blackhole@aptivate.org'
 LOGIN_REDIRECT_URL = "dashboard"
 SITE_NAME = 'Internews Humanitarian Information Dashboard'
-########## END Custom user app defaults
 
-########## BOOTSTRAP3
 BOOTSTRAP3 = {
-
-    # The URL to the jQuery JavaScript file
     'jquery_url': STATIC_URL + '/js/jquery.min.js',
     'javascript_url': STATIC_URL + '/bootstrap/js/bootstrap.min.js',
 
 }
-########## END BOOTSTRAP
 
-########## SLUGLIFIER
-# AUTOSLUG_SLUGIFY_FUNCTION = "slugify.slugify"
-########## END SLUGLIFIER
-
-
-########## LOGGING CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -261,7 +161,6 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            # 'formatter': 'simple'
         },
     },
     'loggers': {
@@ -276,9 +175,7 @@ LOGGING = {
         },
     }
 }
-########## END LOGGING CONFIGURATION
 
-########## DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'DEFAULT_RENDERER_CLASSES': (
@@ -286,16 +183,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
-########## END DJANGO REST FRAMEWORK
 
-########## BINDER STUFF
-# Usually included by adding intranet_binder as a git submodule
-# The name of the class to use to run the test suite
-# TEST_RUNNER = 'intranet_binder.testing.SmartTestSuiteRunner'
-
-########## END BINDER STUFF
-
-# this section allows us to do a deep update of dictionaries
 import collections
 from copy import deepcopy
 
@@ -307,12 +195,6 @@ def update_recursive(dest, source):
         else:
             dest[k] = deepcopy(source[k])
 
-########## LOCAL_SETTINGS
-# tasks.py expects to find local_settings.py so the database stuff is there
-#--------------------------------
-# local settings import
-# from http://djangosnippets.org/snippets/1873/
-#--------------------------------
 try:
     import local_settings
 except ImportError:
@@ -344,17 +226,8 @@ else:
                 globals()[name] = value
         elif re.search('^[A-Z]', attr):
             globals()[attr] = getattr(local_settings, attr)
-########## END LOCAL_SETTINGS
-
-
-##### from here on is stuff that depends on the value of DEBUG
-##### which is set in LOCAL_SETTINGS
-
 
 if DEBUG is False:
-    ########## SITE CONFIGURATION
-    # Hosts/domain names that are valid for this site
-    # See https://docs.djangoproject.com/en/1.8/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = [
         'lin-internewshid.aptivate.org',
         'www.internewshid.aptivate.org',
@@ -363,10 +236,7 @@ if DEBUG is False:
         'internewshid.dev.aptivate.org',
         'internewshid.stage.aptivate.org',
     ]
-    ########## END SITE CONFIGURATION
 
-########## TEMPLATE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
@@ -378,8 +248,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
 
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
 TEMPLATE_DIRS = (
     path.join(BASE_DIR, 'templates'),
 )
@@ -396,8 +264,5 @@ else:
             'django.template.loaders.app_directories.Loader',
         )),
     )
-########## END TEMPLATE CONFIGURATION
 
 CSRF_FAILURE_VIEW = 'hid.views.csrf.csrf_failure'
-
-########## Your stuff: Below this line define 3rd party libary settings
