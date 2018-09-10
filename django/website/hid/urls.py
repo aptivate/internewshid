6171e8291cb93b7ca5d10676f40856eeb09dcfdc
Luke Murphy <lukewm@riseup.net>
2018-09-10 15:43:56 +0200

    Upgrade handling of URLs.

--- django/website/hid/urls.py
+++ django/website/hid/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from dashboard.views import DashboardView
@@ -9,7 +9,7 @@ from .views.item import AddEditItemView
 
 from hid.tabs.view_and_edit_table import view_and_edit_table_form_process_items
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^sources/upload/$', login_required(UploadSpreadsheetView.as_view()), name='sources-upload'),
     url(r'^sources/(?P<label>\w+)/$', login_required(ListSources.as_view()), name='sources-edit'),
     url(r'^sources/$', login_required(ListSources.as_view()), name='sources'),
@@ -17,4 +17,4 @@ urlpatterns = patterns('',
     url(r'^item/(?P<item_id>\d+)/edit/$', login_required(AddEditItemView.as_view()), name='edit-item'),
     url(r'^item/add/(?P<item_type>[-_\w]+)/$', login_required(AddEditItemView.as_view()), name='add-item'),
     url(r'^$', login_required(DashboardView.as_view()), name='dashboard'),
-)
+]
--- django/website/users/urls.py
+++ django/website/users/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import (
     AddContact, UpdateContact, DeleteContact, ListContacts,
@@ -7,8 +7,7 @@ from .views import (
 )
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'edit/$', AddContact.as_view(), name='contact_add'),
     url(r'edit/(?P<pk>\d+)/$', UpdateContact.as_view(),
         name='contact_update'),
@@ -22,4 +21,4 @@ urlpatterns = patterns(
     url(r'export_as_excel/$', ListContactsExport.as_view(), {'format': 'excel'},
         name='contact_list_excel'),
     url(r'$', ListContacts.as_view(), name='contact_list'),
-)
+]
