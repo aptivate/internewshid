9c072415cc515b0273af4257919146d6ef576fb8
Luke Murphy <lukewm@riseup.net>
2018-09-11 11:39:38 +0200

    Make sure we're in UTF-8 mode.

--- django/website/users/tests/factories.py
+++ django/website/users/tests/factories.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import factory
 from django.contrib.auth.models import Group
@@ -12,14 +12,15 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     # must be unique so we specify and increase
     business_email = factory.Sequence(lambda n: "email%d@test.com" % n)
-    first_name = factory.Sequence(lambda n: "ｆíｒѕｔ %d" % n)
-    last_name = factory.Sequence(lambda n: "ｌåｓｔɭａｓｔ %d" % n)
+    first_name = factory.Sequence(lambda n: u"ｆíｒѕｔ %d" % n)
+    last_name = factory.Sequence(lambda n: u"ｌåｓｔɭａｓｔ %d" % n)
 
     # Other required fields (you will still need to handle password
     # yourself depending on what you want to do
     gender = 'female'
-    contact_type = 'ｃòлｔáｃｔ ｔｙｐé'
-    title = 'ｔïｔｌë'
+    contact_type = u"ｃòлｔáｃｔ ｔｙｐé"
+    title = u"ｔïｔｌë"
+
 
 
 def ContactsManagerFactory():
