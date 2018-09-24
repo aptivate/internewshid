19a3012b224890f0e6db44bddbcf6a1bc84aba57
Luke Murphy <lukewm@riseup.net>
2018-09-24 15:20:16 +0200

    Set the default settings file.

--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'internewshid'))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'internewshid'))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
 application = get_wsgi_application()
