a04e189d1d4f2f83986f267c7f1495dbe685dbeb
Luke Murphy <lukewm@riseup.net>
2018-12-05 11:05:12 +0100

    Add a feedback type field.

--- internewshid/data_layer/tests/factories.py
+++ internewshid/data_layer/tests/factories.py
@@ -19,3 +19,8 @@ class ItemFactory(DjangoModelFactory):
     timestamp = fuzzy.FuzzyDateTime(
         timezone.now() + timedelta(days=-365)
     )
+    feedback_type = fuzzy.FuzzyChoice([
+        'Concern',
+        'Question',
+        'Rumour',
+    ])
