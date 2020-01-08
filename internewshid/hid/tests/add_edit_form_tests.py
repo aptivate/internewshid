1275eb80639b5db135e626e9f4ff386b99b344ee
Martin Burchell <martinb@aptivate.org>
2020-01-08 23:10:55 +0000

    Change add/edit form to use age ranges

--- internewshid/hid/tests/add_edit_form_tests.py
+++ internewshid/hid/tests/add_edit_form_tests.py
@@ -5,12 +5,15 @@ from ..forms.item import AddEditItemForm
 
 @patch.object(AddEditItemForm, '_get_category_choices')
 @patch.object(AddEditItemForm, '_get_feedback_type_choices')
+@patch.object(AddEditItemForm, '_get_age_range_choices')
 def test_form_does_not_have_category_field_if_not_defined(
+    age_range_choices,
     feedback_choices,
     category_choices
 ):
     category_choices.return_value = None
     feedback_choices.return_value = None
+    age_range_choices.return_value = None
 
     form = AddEditItemForm()
     assert 'category' not in form.fields
@@ -18,10 +21,13 @@ def test_form_does_not_have_category_field_if_not_defined(
 
 @patch.object(AddEditItemForm, '_get_category_choices')
 @patch.object(AddEditItemForm, '_get_feedback_type_choices')
+@patch.object(AddEditItemForm, '_get_age_range_choices')
 def test_form_has_category_field_if_categories_defined(
+    age_range_choices,
     feedback_choices,
     category_choices
 ):
+    age_range_choices.return_value = None
     feedback_choices.return_value = None
 
     category_choices.return_value = (('', '-----'), ('wash', 'WASH'),)
@@ -31,10 +37,13 @@ def test_form_has_category_field_if_categories_defined(
 
 @patch.object(AddEditItemForm, '_get_category_choices')
 @patch.object(AddEditItemForm, '_get_feedback_type_choices')
+@patch.object(AddEditItemForm, '_get_age_range_choices')
 def test_form_does_not_have_feedback_types_if_not_defined(
+    age_range_choices,
     feedback_choices,
     category_choices
 ):
+    age_range_choices.return_value = None
     category_choices.return_value = None
     feedback_choices.return_value = None
     form = AddEditItemForm()
@@ -77,6 +86,51 @@ def test_form_has_feedback_types_if_defined(
         assert form.fields['feedback_type'].choices == expected_choices
 
 
+@patch.object(AddEditItemForm, '_get_category_choices')
+@patch.object(AddEditItemForm, '_get_feedback_type_choices')
+@patch.object(AddEditItemForm, '_get_age_range_choices')
+def test_form_does_not_have_age_ranges_if_not_defined(
+    age_range_choices,
+    feedback_choices,
+    category_choices
+):
+    category_choices.return_value = None
+    feedback_choices.return_value = None
+    age_range_choices.return_value = None
+    form = AddEditItemForm()
+
+    assert 'age_ranges' not in form.fields
+
+
+@patch.object(AddEditItemForm, '_get_category_choices')
+def test_form_has_age_ranges_if_defined(
+    category_choices
+):
+    with patch('hid.forms.item.transport.terms.list') as term_list:
+        term_list.return_value = [
+            {
+                'taxonomy': 'age-ranges',
+                'name': 'Age 11-14 yrs',
+                'long_name': 'Age 11-14 yrs'
+            },
+            {
+                'taxonomy': 'age-ranges',
+                'name': 'Age 15-18 yrs',
+                'long_name': 'Age 15-18 yrs'
+            },
+        ]
+        expected_choices = [
+            ('', '-----'),
+            ('Age 11-14 yrs', 'Age 11-14 yrs'),
+            ('Age 15-18 yrs', 'Age 15-18 yrs'),
+        ]
+
+        form = AddEditItemForm()
+
+        assert 'age_range' in form.fields
+        assert form.fields['age_range'].choices == expected_choices
+
+
 @patch.object(AddEditItemForm, '_get_feedback_type_choices')
 def test_form_category_has_expected_choices(feedback_choices):
     item_type_category = {
@@ -108,10 +162,13 @@ def test_form_category_has_expected_choices(feedback_choices):
 
 @patch.object(AddEditItemForm, '_get_category_choices')
 @patch.object(AddEditItemForm, '_get_feedback_type_choices')
+@patch.object(AddEditItemForm, '_get_age_range_choices')
 def test_category_field_is_not_required(
+    age_range_choices,
     feedback_choices,
     category_choices
 ):
+    age_range_choices.return_value = None
     feedback_choices.return_value = None
     category_choices.return_value = (('', '-----'), ('wash', 'WASH'),)
 
