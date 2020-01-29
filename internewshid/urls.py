8b6eaa4303f774fc66b7fd81a7df44a87933d72b
Martin Burchell <martinb@aptivate.org>
2020-01-29 11:15:33 +0000

    Fix inclusion of admin.site.urls

--- internewshid/urls.py
+++ internewshid/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('users.auth_urls')),
     url(r'^api/', include('rest_api.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
