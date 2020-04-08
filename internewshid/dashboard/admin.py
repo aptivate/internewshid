1cbf08856e2c31a5046a217889b7141e1e80d93f
Alan M Jackson <alan@aptivate.org>
2020-04-08 12:55:37 +0100

    Changed the covid importer to import more fields and install the json admin editor

--- internewshid/dashboard/admin.py
+++ internewshid/dashboard/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
+from jsoneditor.forms import JSONEditor
+from jsonfield import JSONField
+
 from .models import Dashboard, WidgetInstance
 
 
 class WidgetInstanceInline(admin.StackedInline):
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditor},
+    }
+
     model = WidgetInstance
     extra = 0
 
